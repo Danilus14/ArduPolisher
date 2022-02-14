9d96b2041a93bda93f3bd10e138965a8f3256a58
Daniel <92175316+Danilus14@users.noreply.github.com>
2022-02-14 19:41:17 +0100

    Final menu

--- ArduPolisher/ArduPolisher.ino
+++ ArduPolisher/ArduPolisher.ino
@@ -2,21 +2,47 @@
 #include <LiquidCrystal.h>
 
 /***   Global variables   ***/
+int select=700;
+int up=195;
+int down=380;
+int left=500;
+int right=50;
+int none=1500;
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 int time=0;
 int power=0;
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+int run=0;
+int pointer=0;
+int read;
+int aux;
 
 /***   Function declaration   ***/
-void printLCD ();
+void printPointer ();
+void blinkPointer ();
+void printParams ();
+void printValues ();
 void setParams ();
+void menu ();
 
 void setup()
 {
   pinMode(A0,INPUT);
 
 
+
+
+
+
+
   lcd.begin(16, 2);
 lcd.clear();
+
+
+
+  read=none;
+
+
+  aux=read;
 
 
 
@@ -25,21 +51,88 @@ lcd.clear();
 
 void loop()
 {
-    printLCD();
+    printParams();
     setParams();
 
 }
 
 /***   Function definition   ***/
-void printLCD () {
+void printPointer () {
+  if (pointer == 0) {
+    lcd.setCursor(0,0);
+    lcd.print(">");
+   }else {
+    lcd.setCursor(0,1);
+    lcd.print(">");
+   }
+ }
+void blinkPointer () {
   lcd.setCursor(0,0);
-  lcd.print(String("Time: ") + String(time));
+  lcd.print(" ");
   lcd.setCursor(0,1);
-  lcd.print(String("Power: ") + String(power));
+  lcd.print(" ");
+  delay(500);
+  printPointer();
+  delay(500);
+ }
+void printParams () {
+  lcd.setCursor(1,0);
+  lcd.print("Time: ");
+  lcd.setCursor(1,1);
+  lcd.print("Power: ");
+ }
+void printValues () {
+  lcd.setCursor(7,0);
+  lcd.print(time);
+  lcd.setCursor(7,1);
+  lcd.print(power);
  }
 void setParams () {
-  printLCD();
-  time=analogRead(A0);
-  delay(1000);
-  printLCD();
+  while (run == 0) {
+    read=analogRead(A0);
+    menu();
+    printValues();
+    blinkPointer();
+   }
+ }
+void menu () {
+  if (read != aux) {
+    switch (read)
+    {
+      case select:
+      {
+        run=1;
+        break;
+      }
+      case right:
+      {
+        if (pointer == 0) {
+          time=time + 60;
+         }else {
+          power=power + 10;
+         }
+        break;
+      }
+      case left:
+      {
+        if (pointer == 0) {
+          time=time - 60;
+         }else {
+          power=power - 10;
+         }
+        break;
+      }
+      case up:
+      {
+        pointer=0;
+        break;
+      }
+      case down:
+      {
+        pointer=1;
+        break;
+      }
+    }
+    aux=read;
+   }
  }
